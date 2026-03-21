c37119e3625b6d2fc9e02abbc9d36db3f2d47791
Nathan-Front <jonathantindungan3@gmail.com>
2026-03-21 11:21:54 +0900

    converted all dropdowns to map rendering and added onClick functions

--- src/assets/pages/navigation/mainNav.jsx
+++ src/assets/pages/navigation/mainNav.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import LoginForm from "../forms/loginForm";
 import CreateForm from "../forms/createForm";
 import MobileUserInfo from "../forms/mobileUserInfo";
@@ -115,6 +115,41 @@ function MainNav({ loggedUser, setLoggedUser }) {
       ),
     },
   ];
+
+  const categoryTabs = [
+    {
+      id: "Clothes",
+      label: "Clothes",
+      img: "assets/images/category/still-life-rendering-jackets-display.jpg",
+    },
+    {
+      id: "Shoes",
+      label: "Shoes",
+      img: "assets/images/category/denim-sneakers-street-style.jpg",
+    },
+    {
+      id: "Best offers",
+      label: "Best offers",
+      img: "assets/images/category/couple-backpacks-rocks-sunset.jpg",
+    },
+    {
+      id: "Outdoors",
+      label: "Outdoors",
+      img: "assets/images/category/decorative-cosmetics-accessories-sunglasses-hat-white-table.jpg",
+    },
+  ];
+  const dailyDeals = [
+    {
+      id: "Clothes",
+      label: "Clothes",
+      img: "assets/images/category/still-life-rendering-jackets-display.jpg",
+    },
+    {
+      id: "Shoes",
+      label: "Shoes",
+      img: "assets/images/category/denim-sneakers-street-style.jpg",
+    },
+  ];
   return (
     <>
       <div className="title-cart-panel">
@@ -151,42 +186,6 @@ function MainNav({ loggedUser, setLoggedUser }) {
             <span className="drop-menu-items">News</span>
             <div className="category-button-news">
               <div>
-                {/* <button
-                  type="button"
-                  className={`new-item-display-button ${isActiveTab === "Trending" ? "active-panel active-panel-indicate" : ""}`}
-                  onClick={() => {
-                    setIsActiveTab("Trending");
-                  }}
-                >
-                  Trending item
-                </button>
-                <button
-                  type="button"
-                  className={`new-item-display-button ${isActiveTab === "New" ? "active-panel active-panel-indicate" : ""}`}
-                  onClick={() => {
-                    setIsActiveTab("New");
-                  }}
-                >
-                  New releases
-                </button>
-                <button
-                  type="button"
-                  className={`new-item-display-button ${isActiveTab === "Most" ? "active-panel active-panel-indicate" : ""}`}
-                  onClick={() => {
-                    setIsActiveTab("Most");
-                  }}
-                >
-                  Most ordered
-                </button>
-                <button
-                  type="button"
-                  className={`new-item-display-button ${isActiveTab === "Creative" ? "active-panel active-panel-indicate" : ""}`}
-                  onClick={() => {
-                    setIsActiveTab("Creative");
-                  }}
-                >
-                  Creative styles
-                </button>*/}
                 {tabs.map((tab) => (
                   <button
                     key={tab.id}
@@ -212,156 +211,44 @@ function MainNav({ loggedUser, setLoggedUser }) {
                   {tab.content}
                 </div>
               ))}
-
-              {/*    <div
-                className={`new-item-panel ${isActiveTab === "Trending" ? "active-panel" : ""}`}
-                id={"news-1"}
-              >
-                <div className="news-descript">
-                  <span>Some text</span>
-                  <span>Some text</span>
-                  <span>Some text</span>
-                </div>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Harum dolorem praesentium at alias velit cum. Eius optio
-                  maiores delectus unde ipsam labore a numquam quod aperiam
-                  temporibus, corporis qui accusamus!
-                </p>
-                <img
-                  src="assets/images/category/communication-social-media-icons.jpg"
-                  alt="Trend icon"
-                />
-              </div>
-              <div
-                className={`new-item-panel ${isActiveTab === "New" ? "active-panel" : ""}`}
-                id="news-2"
-              >
-                <p>
-                  Some text here: Lorem ipsum dolor sit, amet consectetur
-                  adipisicing elit. Harum dolorem praesentium at alias velit
-                  cum. Eius optio maiores delectus unde ipsam labore a numquam
-                  quod aperiam temporibus, corporis qui accusamus!
-                </p>
-                <img
-                  src="assets/images/category/couple-backpacks-rocks-sunset.jpg"
-                  alt="New release"
-                />
-              </div>
-              <div
-                className={`new-item-panel ${isActiveTab === "Most" ? "active-panel" : ""}`}
-                id="news-3"
-              >
-                <p>
-                  Some text here: Lorem ipsum dolor sit, amet consectetur
-                  adipisicing elit. Harum dolorem praesentium at alias velit
-                  cum. Eius optio maiores delectus unde ipsam labore a numquam
-                  quod aperiam temporibus, corporis qui accusamus!
-                </p>
-                <img
-                  src="assets/images/category/denim-sneakers-street-style.jpg"
-                  alt="Most ordered"
-                />
-              </div>
-              <div
-                className={`new-item-panel ${isActiveTab === "Creative" ? "active-panel" : ""}`}
-                id="news-4"
-              >
-                <p>
-                  Some text here: Lorem ipsum dolor sit, amet consectetur
-                  adipisicing elit. Harum dolorem praesentium at alias velit
-                  cum. Eius optio maiores delectus unde ipsam labore a numquam
-                  quod aperiam temporibus, corporis qui accusamus!
-                </p>
-                <img
-                  src="assets/images/upper-panel/vecteezy_antique-table-holds-old-literature-rustic-elegance_24642428.jpg"
-                  alt="Creative styles"
-                />
-              </div> */}
             </div>
           </div>
           <div className="category-button">
             <span className="drop-menu-items">Categories</span>
             <div className="category-button-panel">
-              <div>
-                <button
-                  type="button"
-                  className="category-images"
-                  id="clothe-category"
-                >
-                  <span className="dropdown-buttons">Clothes</span>
-                  <img
-                    src="assets/images/category/still-life-rendering-jackets-display.jpg"
-                    alt="category jacket"
-                  />
-                </button>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  className="category-images"
-                  id="shoes-category"
-                >
-                  <span className="dropdown-buttons">Shoes</span>
-                  <img
-                    src="assets/images/category/denim-sneakers-street-style.jpg"
-                    alt="category shoes"
-                  />
-                </button>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  className="category-images"
-                  id="offer-category"
-                >
-                  <span className="dropdown-buttons">Best offers</span>
-                  <img
-                    src="assets/images/category/couple-backpacks-rocks-sunset.jpg"
-                    alt="category bags"
-                  />
-                </button>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  className="category-images"
-                  id="outdoor-category"
-                >
-                  <span className="dropdown-buttons">Outdoors</span>
-                  <img
-                    src="assets/images/category/decorative-cosmetics-accessories-sunglasses-hat-white-table.jpg"
-                    alt="category accessories"
-                  />
-                </button>
-              </div>
+              {categoryTabs.map((cTab) => (
+                <div key={cTab.id}>
+                  <button
+                    type="button"
+                    className="category-images"
+                    onClick={() => {
+                      alert("Link to " + cTab.id);
+                    }}
+                  >
+                    <span className="dropdown-buttons">{cTab.label}</span>
+                    <img src={cTab.img} alt={`category ${cTab.label}`} />
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
           <div className="daily-button">
             <span className="drop-menu-items">Daily deals</span>
             <div className="daily-button-panel">
-              <div>
-                <button
-                  type="button"
-                  className="daily-images"
-                  id="clothe-daily"
-                >
-                  <span className="dropdown-buttons">Clothes</span>
-                  <img
-                    src="assets/images/category/still-life-rendering-jackets-display.jpg"
-                    alt="category jacket"
-                  />
-                </button>
-              </div>
-              <div>
-                <button type="button" className="daily-images" id="shoes-daily">
-                  <span className="dropdown-buttons">Shoes</span>
-                  <img
-                    src="assets/images/category/denim-sneakers-street-style.jpg"
-                    alt="category shoes"
-                  />
-                </button>
-              </div>
+              {dailyDeals.map((dTab) => (
+                <div key={dTab.id}>
+                  <button
+                    type="button"
+                    className="daily-images"
+                    onClick={() => {
+                      alert("Link to " + dTab.id);
+                    }}
+                  >
+                    <span className="dropdown-buttons">{dTab.label}</span>
+                    <img src={dTab.img} alt={`category ${dTab.label}`} />
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
           <div className="logged-in-user">
